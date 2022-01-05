b0f34b61446d6d4a86b28b29e4fcca18e4dd4741
akua21 <albareinders@gmail.com>
2022-01-05 19:04:12 +0100

    s4 done

---
+++ s4/src/models/model.py
@@ -0,0 +1,39 @@
+from torch import nn, optim
+
+from pytorch_lightning import LightningModule
+
+class MyAwesomeModel(LightningModule):
+    def __init__(self, hidden_layer, activation, last_activation, output_shape, lr):
+        super().__init__()
+        self.hidden = nn.Linear(hidden_layer[0], hidden_layer[1])
+        self.output = nn.Linear(hidden_layer[1], output_shape)
+        self.sigmoid = eval("nn." + activation)
+        self.softmax = eval("nn." + last_activation)
+        self.flatten = nn.Flatten()
+
+        self.lr = lr
+        self.criterium = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        x = self.softmax(x)
+        return x
+
+    def last_layer_features(self, x):
+        x = self.flatten(x)
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        target = target.long()
+        loss = self.criterium(preds, target)
+        return loss
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.lr)
---
+++ s4/src/visualization/visualize.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn
+import torch
+from sklearn.manifold import TSNE
+from tqdm import tqdm
+
+from src.models import MyAwesomeModel
+
+seaborn.set()
+
+model = MyAwesomeModel()
+model.load_state_dict(torch.load("models/mnist/trained_model.pt"))
+model.eval()
+
+
+train_set = torch.load("data/processed/corruptmnist/train.pt")
+
+features = []
+colors = []
+
+for images, labels in tqdm(train_set):
+    out = model.last_layer_features(images)
+
+    features.extend(out.detach().numpy())
+    colors.extend(labels.detach().numpy() * 28 / 255)
+
+tsne = TSNE(n_components=2).fit_transform(features)
+
+tx = tsne[:, 0]
+ty = tsne[:, 1]
+
+tx = (tx - tx.min()) / (tx.max() - tx.min())
+ty = (ty - ty.min()) / (ty.max() - ty.min())
+
+
+plt.figure(figsize=(8, 8))
+plt.title("Train features visualization")
+
+
+plt.scatter(tx, ty, c=colors)
+
+
+plt.savefig("reports/figures/visualization.png")
+plt.close()
